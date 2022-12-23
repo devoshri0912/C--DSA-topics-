31fca7752d36a017c5424d6549d2e20b7584a98c
Devoshree Shrivastava <devoshree.shrivastava09@gmail.com>
2022-12-24 03:08:34 +0530

    Create SubsequenceOfString.cpp

---
+++ recurssion/SubsequenceOfString.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(vector<string>& ans, string str, string output, int i) {
+    //base case
+    if(i>=str.length()) {
+        if(output.length()>0)
+        ans.push_back(output);
+        return ;
+        }
+    
+    //exclude
+    solve(ans, str, output, i+1);
+    //include
+    output.push_back(str[i]);
+    solve(ans, str, output, i+1);
+}
+
+vector<string> subsequences(string str){
+	
+	vector<string> ans;
+    string output = "";
+    solve(ans,str,output,0);
+    return ans;
+	
+}
+int main()
+{
+    string str="devoshree";
+    vector<string> rev=subsequences(str);
+    for(int i=0;i<rev.size();i++)
+    {
+        for(int j=0;j<rev[i].size();j++)
+        cout<<rev[i][j]<<" ";
+
+        cout<<endl;
+    }
+}
